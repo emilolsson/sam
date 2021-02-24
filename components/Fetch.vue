f4a68c2008aa5b2200513ac1ee101db9f5aae2f8
Emil Olsson <hi@emilolsson.com>
2021-02-24 16:56:29 +0100

    message

---
+++ components/Fetch.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="Fetch" class="Fetch col col-100">
+
+    <div v-html="document.data">
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    document: { data: { intro_text: null } },
+  }),
+  async fetch() {
+      try {
+        this.document = await this.$prismic.client.getSingle('homepage')
+      } catch {
+        this.$nuxt.error({ statusCode: 404, message: "Data not found" });
+      }
+  },
+}
+</script>
+
+<style lang="scss">
+.Fetch {
+  background: blue;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <Fetch />
+  </div>
+</template>
+
+<script>
+import Fetch from '~/components/Fetch.vue'
+
+export default {
+  components: {
+    Fetch,
+  },
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
